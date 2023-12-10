90424e81bbadd9caa6426d129e98387d62314cd5
carrod23daw2l <15587030.clot@fje.edu>
2023-12-11 00:15:58 +0100

    Versió 1.2 del projecte prjava02

--- Prjava02.java
+++ Prjava02.java
@@ -26,7 +26,9 @@ public class Prjava02 {
             System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
             System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
             System.out.println("Versió OS: " + System.getProperty("os.version"));
-            System.out.println("Creació d'una branca del projecte prjava02"); // line inserted by carodu
+            System.out.println("Creació d'una branca del projecte prjava02"); // line inserted by carodu            
+            System.out.println("Afegint més codi des de la branca00 del projecte prjava02"); // line inserted after testing branca00
+
         }
         catch (IOException e) {
 		System.out.println("Exception occurred");
